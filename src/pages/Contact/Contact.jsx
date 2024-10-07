80fba35463339e546db0da868af91a3325e273f6
shivanirajani <c1015654@newcastle.ac.uk>
2024-10-07 11:29:28 +0100

    fix this

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,67 +1,60 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import { FaRegEnvelope } from 'react-icons/fa';
 import emailjs from '@emailjs/browser';
 
 const Contact = () => {
   const form = useRef();
-  const [feedback, setFeedback] = useState('');
 
   const sendEmail = (e) => {
     e.preventDefault();
 
     emailjs
-      .sendForm('service_kfxmgnh', 'template_3rvwo94', form.current, {
-        user_id: '4ufXFHOu330069y8T', // Ensure to use the correct user_id
-      })
+    .sendForm('service_kfxmgnh', 'template_3rvwo94', form.current, {
+      publicKey: '4ufXFHOu330069y8T',
+    })
       .then(
         (result) => {
           console.log('SUCCESS!', result.text);
-          setFeedback('Email sent successfully!');
-          form.current.reset();
         },
         (error) => {
           console.log('FAILED...', error.text);
-          setFeedback('Failed to send email. Please try again.');
         }
       );
+
+    // Clear the form after submission if needed
+    form.current.reset();
   };
 
   return (
     <section className="contact" data-page="contact">
       <header>
-        <h2 className="h2 article-title">Contact</h2>
+        <h2 className="h2 article-title">Contacto</h2>
       </header>
 
       <section className="mapbox" data-mapbox>
         <figure>
-          <iframe 
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d73226.62573739623!2d-1.7394154944650821!3d55.00234419516514!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487d857e0c6f64cd%3A0xbe252b072a76191!2sNewcastle%20upon%20Tyne!5e0!3m2!1sen!2suk!4v1728296201843!5m2!1sen!2suk" 
-            width="600" 
-            height="450" 
-            style={{ border: 0 }} 
-            allowFullScreen 
-            loading="lazy" 
-            referrerPolicy="no-referrer-when-downgrade" 
-            title="Map of Newcastle upon Tyne"
-          />
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d56303.26511151111!2d-15.480816846771004!3d28.117430296106967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xc40950e91c097d1%3A0xab36b5ac5338ba65!2sLas%20Palmas%20de%20Gran%20Canaria%2C%20Las%20Palmas!5e0!3m2!1sen!2ses!4v1713660725635!5m2!1sen!2ses"
+            width="400" height="300" loading="lazy" title="Google Map"
+          ></iframe>
         </figure>
       </section>
 
       <section className="contact-form">
         <form className="form" onSubmit={sendEmail} ref={form}>
           <div className="input-wrapper">
-            <input type="text" name="nombre" className="form-input" placeholder="Name" required />
-            <input type="text" name="apellidos" className="form-input" placeholder="Surname" required />
-            <input type="email" name="email" className="form-input" placeholder="Email" required />
-            <input type="tel" name="telefono" className="form-input" placeholder="Phone Number" required />
+            <input type="text" name="nombre" className="form-input" placeholder="Nombre" required />
+            <input type="text" name="apellidos" className="form-input" placeholder="Apellidos" required />
+            <input type="email" name="email" className="form-input" placeholder="Correo Electrónico" required />
+            <input type="tel" name="telefono" className="form-input" placeholder="Teléfono" required />
           </div>
 
-          <textarea name="asunto" className="form-input" placeholder="Subject" required></textarea>
-          <textarea name="mensaje" className="form-input" placeholder="Message" required></textarea>
+          <textarea name="asunto" className="form-input" placeholder="Asunto" required></textarea>
+          <textarea name="mensaje" className="form-input" placeholder="Mensaje" required></textarea>
 
           <button className="form-btn" type="submit">
             <FaRegEnvelope />
-            <span>Submit</span>
+            <span>Enviar</span>
           </button>
         </form>
       </section>
