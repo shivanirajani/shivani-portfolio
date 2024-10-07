a109dd5dd884446bf573e9032a43d3b30847557c
shivanirajani <c1015654@newcastle.ac.uk>
2024-10-07 11:33:20 +0100

    changes

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -33,28 +33,34 @@ const Contact = () => {
 
       <section className="mapbox" data-mapbox>
         <figure>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d56303.26511151111!2d-15.480816846771004!3d28.117430296106967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xc40950e91c097d1%3A0xab36b5ac5338ba65!2sLas%20Palmas%20de%20Gran%20Canaria%2C%20Las%20Palmas!5e0!3m2!1sen!2ses!4v1713660725635!5m2!1sen!2ses"
-            width="400" height="300" loading="lazy" title="Google Map"
-          ></iframe>
+        <iframe 
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d73226.62573739623!2d-1.7394154944650821!3d55.00234419516514!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487d857e0c6f64cd%3A0xbe252b072a76191!2sNewcastle%20upon%20Tyne!5e0!3m2!1sen!2suk!4v1728296201843!5m2!1sen!2suk" 
+            width="600" 
+            height="450" 
+            style={{ border: 0 }} 
+            allowFullScreen 
+            loading="lazy" 
+            referrerPolicy="no-referrer-when-downgrade" 
+            title="Map of Newcastle upon Tyne"
+          />
         </figure>
       </section>
 
       <section className="contact-form">
         <form className="form" onSubmit={sendEmail} ref={form}>
           <div className="input-wrapper">
-            <input type="text" name="nombre" className="form-input" placeholder="Nombre" required />
-            <input type="text" name="apellidos" className="form-input" placeholder="Apellidos" required />
-            <input type="email" name="email" className="form-input" placeholder="Correo Electrónico" required />
-            <input type="tel" name="telefono" className="form-input" placeholder="Teléfono" required />
+            <input type="text" name="nombre" className="form-input" placeholder="Name" required />
+            <input type="text" name="apellidos" className="form-input" placeholder="Surname" required />
+            <input type="email" name="email" className="form-input" placeholder="Email" required />
+            <input type="tel" name="telefono" className="form-input" placeholder="Phone Number" required />
           </div>
 
-          <textarea name="asunto" className="form-input" placeholder="Asunto" required></textarea>
-          <textarea name="mensaje" className="form-input" placeholder="Mensaje" required></textarea>
+          <textarea name="asunto" className="form-input" placeholder="Subject" required></textarea>
+          <textarea name="mensaje" className="form-input" placeholder="Message" required></textarea>
 
           <button className="form-btn" type="submit">
             <FaRegEnvelope />
-            <span>Enviar</span>
+            <span>Submit</span>
           </button>
         </form>
       </section>
