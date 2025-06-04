97029def9c5a285b64850ae2e00ef4bab9056abc
shivanirajani <c1015654@newcastle.ac.uk>
2025-06-04 11:55:36 +0100

    change map

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -98,18 +98,22 @@ const Contact = () => {
       </header>
 
       <section className="mapbox" data-mapbox>
-        <figure>
-          <iframe
-            src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d73226.62573739623!2d-1.7394154944650821!3d55.00234419516514!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487d857e0c6f64cd%3A0xbe252b072a76191!2sNewcastle%20upon%20Tyne!5e0!3m2!1s${language}!2s${language}!4v1728296201843!5m2!1s${language}!2s${language}`}
-            width="600"
-            height="450"
-            style={{ border: 0 }}
-            allowFullScreen
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-            title={language === "es" ? "Mapa de Newcastle upon Tyne" : "Map of Newcastle upon Tyne"}
-          />
-        </figure>
+
+      <figure>
+  <iframe
+    src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d389392.3051817583!2d-15.84713117499756!3d28.056942601993295!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xc40950e91c097d1%3A0xab36b5ac5338ba65!2sLas%20Palmas%20de%20Gran%20Canaria!5e0!3m2!1s${language}!2s${language}!4v1748986976534!5m2!1s${language}!2s${language}`}
+    width="600"
+    height="450"
+    style={{ border: 0 }}
+    allowFullScreen
+    loading="lazy"
+    referrerPolicy="no-referrer-when-downgrade"
+    title={language === "es" ? "Mapa de Las Palmas de Gran Canaria" : "Map of Las Palmas de Gran Canaria"}
+  />
+</figure>
+
+
+
       </section>
 
       <section className="contact-form">
